4ac16a05bfbf9214537a72b0e9c7f151ca1edb24
Carissnm <carolinapettinaroli@gmail.com>
2022-08-12 21:05:31 -0300

    agrego modales y logica de validaciones

--- ahorcado.js
+++ ahorcado.js
@@ -5,27 +5,35 @@ const btnNuevoJuego = document.querySelector('.btn-nuevojuego');
 const btnVolver = document.querySelector('.btn-volver');
 const btnGuardarPalabra = document.querySelector('.btn-guardar');
 const btnCancelar = document.querySelector('.btn-cancelar');
+const btnSeguirGuardando = document.querySelector('.btn-guardar-seguir');
+const btnCancelarModal = document.querySelector('.cancelar-modal');
+const btnContinuarModal = document.querySelector('.continuar-modal');
+const btnInicioModal = document.querySelector('.inicio-modal');
+const btnVolverModal = document.querySelector('.volver-jugar-modal');
 const nuevaPalabra = document.querySelector('#nueva-palabra');
+const textarea = document.querySelector('#nueva-palabra');
 const seccionInicio = document.querySelector('.botonera-inicio');
 const seccionPalabrasNuevas = document.querySelector('.palabras-nuevas');
 const seccionAhorcado = document.querySelector('.juego-ahorcado');
-const textarea = document.querySelector('#nueva-palabra');
-const btnSeguirGuardando = document.querySelector('.btn-guardar-seguir');
+
+
 const modal = document.querySelector('.modal');
-const btnModalCancelar = document.querySelector('.cancelar-modal');
+const modalGanaste = document.querySelector('.modal-ganaste');
+const modalAviso = document.querySelector('.modal-aviso');
 
 
 
 
 //VARIABLES
-let palabras = ['PARTIDO', 'PEREZOSO', 'APARATO', 'COMPUTOS', 'TALADRO', 'PELOTA', 'SASTRE', 'SERENO', 'POLIEDRO', 'TREX', 'LUNA', 'HIPOTECA', 'CELESTE', 'ATENCION', 'DESLIZ', 'HELADO', 'CARTAS', 'CEREZA', 'RUGBY', 'FIESTA', 'TRAQUEA', 'FUNCION', 'MARTILLO', 'MISTERIO', 'CAMION', 'CULO']
+let palabras = ['PARTIDO', 'PEREZOSO', 'APARATO', 'COMPUTOS', 'TALADRO', 'PELOTA', 'SASTRE', 'SERENO', 'POLIEDRO', 'TREX', 'LUNA', 'HIPOTECA', 'CELESTE', 'ATENCION', 'DESLIZ', 'HELADO', 'CARTAS', 'CEREZA', 'RUGBY', 'FIESTA', 'TRAQUEA', 'FUNCION', 'MARTILLO', 'MISTERIO', 'CAMION', 'CULO', 'CIERVO', 'SANDWICH', 'ROBLOX', 'GATO', 'MOTONETA', 'LANCHA', 'PEZ', 'ESCUELA', 'FAMILIA', 'AMISTAD']
 
-let palabra = '';
+let palabraSecreta = '';
 let intentos = 0;
 
 
 //EVENTOS
-
+btnInicioModal.onclick = irInicio;
+btnVolverModal.onclick = seguirJugandoModal;
 btnIniciar.onclick = iniciarJuego;
 btnAgregar.onclick = irSeccionAgregar;
 btnVolver.onclick = volverInicio;
@@ -33,18 +41,23 @@ btnGuardarPalabra.onclick = guardarYEmpezar;
 btnCancelar.onclick = cancelar;
 btnNuevoJuego.onclick = jugarDeNuevo;
 btnSeguirGuardando.onclick = guardar;
-btnModalCancelar.onclick = mostrarOcultarMensajePerder;
+btnCancelarModal.onclick = volverYCerrarModal;
+btnContinuarModal.onclick = jugarYCerrarModal;
 
 // FUNCIONES
 function iniciarJuego() {
     seccionInicio.classList.add('escondido');
     seccionAhorcado.classList.remove('escondido');
     sortearPalabra();
+    toggleModalAviso();
+    setTimeout(toggleModalAviso, 3000);
 }
 
 function irSeccionAgregar() {
     seccionInicio.classList.add('escondido');
-    seccionPalabrasNuevas.classList.remove('escondido')
+    seccionPalabrasNuevas.classList.remove('escondido');
+    toggleModalAviso();
+    setTimeout(toggleModalAviso, 3000);
 }
 
 function volverInicio() {
@@ -54,6 +67,35 @@ function volverInicio() {
     resetLetras();
     resetearAhorcado();
     
+}
+
+function volverYCerrarModal() {
+    volverInicio()
+    toggleModalPerdiste()
+}
+
+function jugarYCerrarModal() {
+    toggleModalPerdiste()
+    resetearAhorcado();
+    resetLetras();
+    resetearPalabra();
+    sortearPalabra();
+}
+
+function irInicio() {
+    toggleModalGanaste();
+    volverInicio();
+    resetLetras();
+    resetearAhorcado();
+    resetearPalabra();
+}
+
+function seguirJugandoModal() {
+    toggleModalGanaste();
+    resetLetras();
+    resetearAhorcado();
+    resetearPalabra();
+    sortearPalabra();
 }
 
 function guardarYEmpezar() {
@@ -87,22 +129,25 @@ function limpiarTextarea() {
 
 function sortearPalabra(){
     const letterboxes = document.querySelector('.inputs');
-    let posicionAleatoria = Math.round(Math.random()*palabras.length)
-    let palabra = palabras[posicionAleatoria];
-    for (let letra of palabra) {
+    let posicionAleatoria = Math.floor(Math.random()*palabras.length)
+    palabraSecreta = palabras[posicionAleatoria];
+    for (let letra of palabraSecreta) {
         let box = document.createElement('div');
         box.className = 'contenedor-box'
         box.innerHTML = `<input data-id=${letra} class="box" type="text" disabled>`
         letterboxes.appendChild(box);
     }
     ;
-    console.log(palabra)
+    console.log(palabraSecreta)
     adivinarPalabra();
 }
 
 function adivinarPalabra() {
     let listaLetrasEquivocadas = []
     const letrasError = document.querySelector('.letra');
+    const inputs = document.querySelectorAll('.box')
+    let contador = 0;
+    let palabra = [];
 
     document.onkeypress = e => {
         const inputs = document.querySelectorAll('.box')
@@ -110,13 +155,17 @@ function adivinarPalabra() {
         if(/[A-Z]/.test(tecla) && !/[\W_]/.test(tecla) && tecla !== 'Alt' && tecla !== 'CapsLock' && tecla !== 'Shift' && tecla !== 'Control') {
             
 
-            inputs.forEach(data => {
+            inputs.forEach((data, i) => {
                 if(tecla === data.getAttribute('data-id')) {
                     data.value = tecla;
                     data.className = 'letra-visible box';
-                    console.log(tecla)
+
+                    palabra[i] = data.value
+                    if(palabra.join('') === palabraSecreta) {
+                            setTimeout(toggleModalGanaste, 300)
+                    }
                 } else {
-                    if(!listaLetrasEquivocadas.includes(tecla) && !palabra.includes(tecla)){
+                    if(!listaLetrasEquivocadas.includes(tecla) && !palabraSecreta.includes(tecla)){
                         listaLetrasEquivocadas.push(tecla)
                         const letraError = document.createElement('p');
                         letraError.className = 'letra-error-parrafo';
@@ -134,6 +183,10 @@ function adivinarPalabra() {
         
     };
     
+    
+    if(contador === inputs.length) {
+        modalGanaste.classList.remove('escondido');
+    }
 } 
 
 function dibujarAhorcado(intentos) {
@@ -164,7 +217,7 @@ function dibujarAhorcado(intentos) {
             break;
         case 9 :
             document.querySelector('.leg-r').classList.remove('escondido');
-            mostrarOcultarMensajePerder();
+            setTimeout(toggleModalPerdiste, 300)
             break;
     }
 }
@@ -194,12 +247,46 @@ function resetearAhorcado() {
     })
 }
 
-function mostrarOcultarMensajePerder() {
-    if(!modal.classList.contains('escondido')){
-        modal.classList.add('escondido');
+function toggleModalPerdiste() {
+    const divModal = document.querySelector('.center');
+    if(divModal.classList.contains('popup')) {
+        divModal.classList.remove('popup');
     } else {
-        modal.classList.remove('escondido')
+        divModal.classList.add('popup')
+    }
+    if(modal.classList.contains('escondido')) {
+        modal.classList.remove('escondido');
+    } else {
+        modal.classList.add('escondido');
+    }
+}
+
+function toggleModalGanaste() {
+    const divModalGanaste = document.querySelector('.ganaste')
+    if(divModalGanaste.classList.contains('popup')){
+        divModalGanaste.classList.remove('popup');
+    } else {
+        divModalGanaste.classList.add('popup');
+    }
+    if(modalGanaste.classList.contains('escondido')) {
+        modalGanaste.classList.remove('escondido');
+    } else {
+        modalGanaste.classList.add('escondido');
     }
 }
 
 
+function toggleModalAviso() {
+    const divModalAviso = document.querySelector('.aviso');
+    if(divModalAviso.classList.contains('popup')) {
+        divModalAviso.classList.remove('popup');
+    } else {
+        divModalAviso.classList.add('popup');
+    }
+    
+    if(modalAviso.classList.contains('escondido')) {
+        modalAviso.classList.remove('escondido');
+    } else {
+        modalAviso.classList.add('escondido');
+    }
+}
